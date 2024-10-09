e989c574a672bd3dce8be7c6ae48ffe99f21ddb0
Chriscastanuela <chriscastanuela@ymail.com>
2024-10-09 17:10:42 -0600

    fix

--- api/index.js
+++ api/index.js
@@ -13,8 +13,8 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const LIST_ID_TEXT = process.env.LIST_ID_TEXT || "901105262068"; // Fallback list ID for texts
-const LIST_ID_VOICEMAIL = process.env.LIST_ID_VOICEMAIL || "901105537156"; // Fallback list ID for voicemails
+const LIST_ID_TEXT = process.env.TEXT_LIST_ID || "901105537156"; // Fallback list ID for texts
+const LIST_ID_VOICEMAIL = "901105262068"; // List ID for voicemails
 
 // Helper function for making HTTPS requests
 const makeApiRequest = (options, postData = null) => {
